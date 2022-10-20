f760649859579b947aad1add85e7ab59b8ac3a8d
fahiid33 <trey34809@gmail.com>
2022-10-20 23:07:49 +0100

    init

--- parsing/parse.h
+++ parsing/parse.h
@@ -1,14 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/20 23:06:17 by fstitou           #+#    #+#             */
-/*   Updated: 2022/10/20 23:06:22 by fstitou          ###   ########.fr       */
+/*   Created: 2022/10/20 23:06:47 by fstitou           #+#    #+#             */
+/*   Updated: 2022/10/20 23:06:49 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parse.h"
+#ifndef PARSE_H
+#define PARSE_H
 
+#include <stdlib.h> 
+#include "GNL/get_next_line.h"
+#include <unistd.h>
+#include <stdio.h>
+
+// init
+
+
+
+
+
+
+#endif
