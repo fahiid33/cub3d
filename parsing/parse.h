0ebba8c64d21689ba81d891a4e74c244b8d24c90
fahiid33 <trey34809@gmail.com>
2022-10-21 05:08:16 +0100

    in progress

--- parsing/parse.h
+++ parsing/parse.h
@@ -6,7 +6,7 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 23:06:47 by fstitou           #+#    #+#             */
-/*   Updated: 2022/10/21 01:35:33 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/10/21 03:36:43 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,20 @@
 
 // init
 
-
+typedef struct s_parse
+{
+    int NO;
+    int SO;
+    int WE;
+    int EA;
+    int R;
+    int floor;
+    int ceil;
+    int inv_line;
+    int wg_char;
+    int no_pos;
+    int no_map;
+}   t_parse;
 
 
 #endif
