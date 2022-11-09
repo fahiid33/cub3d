b5da1111f22a7d4feb4795acc8a709f4e5ced1db
hassan khalil <hkhalil@student.1337.ma>
2022-11-09 02:10:36 +0100

    norm

--- execution/main.c
+++ execution/main.c
@@ -3,24 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 01:58:49 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/09 01:54:10 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/11/09 02:09:40 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-//use get_inter one time
+void	game_init(t_data *game, t_info *info)
+{
+	game->map = info->new_map;
+	game->map_rows = info->map_y;
+	game->map_columns = info->map_x;
+	game->map_length = info->map_y * CUBE;
+	game->map_width = info->map_x * CUBE;
+	game->player_x = info->x * CUBE + CUBE / 2;
+	game->player_y = info->y * CUBE + CUBE / 2;
+	game->floor = info->floor;
+	game->ceilling = info->ceil;
+	game->angle = ft_angle(info->dir);
+	game->mlx = mlx_init();
+	game->mlx_window = mlx_new_window(game->mlx, RX, RY, "cub3d");
+	game->textures[0].t_file = info->no;
+	game->textures[1].t_file = info->so;
+	game->textures[2].t_file = info->we;
+	game->textures[3].t_file = info->ea;
+	free_struct(info);
+	read_texture(game);
+	render_walls(game);
+	render_map(game);
+	mlx_put_image_to_window(game->mlx, game->mlx_window, game->img, 0, 0);
+}
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	char	**tab;
 	t_info	*info;
+	t_data	*game;
 	int		fd;
 
-	//parsing
 	fd = 0;
 	(void)ac;
 	tab = init_file(av[1], fd);
@@ -32,38 +55,8 @@ int main(int ac, char **av)
 	}
 	info = fill_infos(tab);
 	free_tab(tab);
-	// system("leaks cub3D");
-	// exit(0);
-	//execution
-	t_data *game = malloc(sizeof(t_data));
-	game->map = info->new_map;
-	game->map_rows = info->map_y;
-	game->map_columns = info->map_x;
-	game->map_length = info->map_y * CUBE;
-	game->map_width = info->map_x * CUBE;
-	game->player_x = info->x*CUBE + CUBE/2;
-	game->player_y = info->y*CUBE + CUBE/2;
-	game->floor = info->floor;
-	game->ceilling = info->ceil;
-	game->angle = ft_angle(info->dir);
-	game->mlx = mlx_init();
-	game->mlx_window = mlx_new_window(game->mlx, RX, RY, "cub3d");
-	//n
-	game->textures[0].t_file = info->no;
-	//s
-	game->textures[1].t_file = info->so;
-	// w
-	game->textures[2].t_file = info->we;
-	//e
-	game->textures[3].t_file = info->ea;
-	//free info struct here
-	free_struct(info);
-	//debug
-	read_texture(game);
-	//end debug
-	render_walls(game);
-	render_map(game);
-	mlx_put_image_to_window(game->mlx, game->mlx_window, game->img, 0, 0);
+	game = malloc(sizeof(t_data));
+	game_init(game, info);
 	mlx_hook(game->mlx_window, 17, 0, ft_exit, game);
 	mlx_hook(game->mlx_window, 2, 0, key_hook, game);
 	mlx_mouse_hook(game->mlx_window, mouse_hook, game);
